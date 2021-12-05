874f67ee61fcaa9f03221878512f137de3bd9aba
heraldjose10 <heraldjose10@gmail.com>
2021-12-06 00:15:32 +0530

    add navbar buttons

---
+++ src/components/side-bar-btns-group/side-bar-btns-group.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import NavItem from "../nav-item/nav-item.component";
+
+import './side-bar-btns-group.styles.scss';
+
+class SideBarBtnsGroup extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      buttons: [
+        { name: 'Create Playlist', iconUrl: '', to: 'add-playlist' },
+        { name: 'Liked Songs', iconUrl: '', to: 'liked-songs' }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='side-bar-btns-group'>
+        {
+          this.state.buttons.map((button) => (
+            <NavItem {...button} />
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default SideBarBtnsGroup;
--- src/components/side-bar/side-bar.component.jsx
+++ src/components/side-bar/side-bar.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import SideBarBtnsGroup from "../side-bar-btns-group/side-bar-btns-group.component";
 import SideBarNav from "../side-bar-nav/side-bar-nav.component";
 
 import './side-bar.styles.scss'
 
 const SideBar = () => (
   <div className='side-bar'>
-    <SideBarNav/>
+    <SideBarNav />
+    <SideBarBtnsGroup />
+    <hr/>
   </div>
 )
 
