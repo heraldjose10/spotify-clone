ebeedb08218f953657351908f7235feec62cb58d
heraldjose10 <heraldjose10@gmail.com>
2021-12-20 22:31:04 +0530

    fix bugs with sidebar playlists and likedsongs banner

--- src/components/playlist-banner/playlist-banner.component.jsx
+++ src/components/playlist-banner/playlist-banner.component.jsx
@@ -2,38 +2,42 @@ import React from "react";
 
 import './playlist-banner.styles.scss'
 
-const PlaylistBanner = ({details}) => {
+const PlaylistBanner = ({ details }) => {
 
-  const { name, description, imageUrl, followers, createdBy, totalSongs } = details
-
-  return (
-    <div className='banner'>
-    {
-      imageUrl === 'liked'
-        ? <div className='playlist-image'>
-          <i className='bi bi-heart-fill'></i>
-        </div>
-        : <img className='playlist-image' alt='heart icon' src={imageUrl} />
-    }
-    <div className='playlist-info'>
-      <p>PLAYLIST</p>
-      <h1>{name}</h1>
-      {
-        description
-          ? <p className='description'>{description}</p>
-          : ''
-      }
-      <p>
+  if (details) {
+    const { name, description, imageUrl, followers, createdBy, totalSongs } = details
+    return (
+      <div className='banner'>
         {
-          `${createdBy} | ${totalSongs} songs${followers
-            ? ` | ${followers} likes`
-            : ''
-          }`
+          imageUrl === 'liked'
+            ? <div className='playlist-image'>
+              <i className='bi bi-heart-fill'></i>
+            </div>
+            : <img className='playlist-image' alt='heart icon' src={imageUrl} />
         }
-      </p>
-    </div>
-    </div>
-  )
+        <div className='playlist-info'>
+          <p>PLAYLIST</p>
+          <h1>{name}</h1>
+          {
+            description
+              ? <p className='description'>{description}</p>
+              : ''
+          }
+          <p>
+            {
+              `${createdBy} | ${totalSongs} songs${followers
+                ? ` | ${followers} likes`
+                : ''
+              }`
+            }
+          </p>
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (<h1>Loading</h1>)
+  }
 }
 
 export default PlaylistBanner
--- src/components/sidebar-playlists-list/sidebar-playlists-list.component.jsx
+++ src/components/sidebar-playlists-list/sidebar-playlists-list.component.jsx
@@ -1,11 +1,29 @@
 import { connect } from "react-redux";
 import React from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+import { setPlaylists } from "../../redux/playlist/playlist.actions";
+import { API_ENDPOINT } from "../../endpoints";
 
 import './sidebar-playlists-list.styles.scss'
 
 
-const SideBarPlaylistsList = ({ playlists }) => {
+const SideBarPlaylistsList = ({ playlists, setPlaylists, token, userId }) => {
+
+  useEffect(() => {
+    if (playlists.length === 0) {
+      const getUserPlaylists = async (userId, userToken) => {
+        let response = await axios(`${API_ENDPOINT}users/${userId}/playlists`, {
+          headers: { Authorization: `Bearer ${userToken}` },
+        })
+        setPlaylists(response.data.items)
+      }
+      getUserPlaylists(userId, token)
+    }
+  }, [playlists, userId, token, setPlaylists])
+
   return (
     <div className='list'>
       {
@@ -22,7 +40,13 @@ const SideBarPlaylistsList = ({ playlists }) => {
 }
 
 const mapStateToProps = state => ({
-  playlists: state.playlist.playlists
+  playlists: state.playlist.playlists,
+  token: state.user.currentUser.token,
+  userId: state.user.currentUser.id
 })
 
-export default connect(mapStateToProps)(SideBarPlaylistsList);
+const mapDispatchToProps = dispatch => ({
+  setPlaylists: playlists => dispatch(setPlaylists(playlists))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SideBarPlaylistsList);
